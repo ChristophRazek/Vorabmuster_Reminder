a7139b4ecc94af568417a641f453412b3be2ca66
ChristophRazek <52331031+ChristophRazek@users.noreply.github.com>
2024-02-01 15:09:51 +0100

    Release 1

--- main.py
+++ main.py
@@ -16,27 +16,45 @@ df_samples = u.update()
 
 def send_reminder(df_samples):
 
-    receivers = {'NUCO': 'yian.su@emea-cosmetics.com','ANCOROTTI': 'yian.su@emea-cosmetics.com','ART': 'yian.su@emea-cosmetics.com'}
+    receivers = {'NUCO': ['a.adamczewska@nuco.pl', 'k.jeziorska@nuco.pl'],
+                 'ANCOROTTI': ['vbonazza@ancorotticosmetics.com', 'abrambini@ancorotticosmetics.com', 'smonticelli@ancorotticosmetics.com'],
+                 'ART': ['chiara.bonacina@artcosmetics.it', 'valentina.ghillini@artcosmetics.it'],
+                 'CHROMAVIS':'magdalena.skupinska@chromavis.com',
+                 'CONFALON':'sonia@confaloniericosmetica.com',
+                 'COSMETEC':['balzarienrica@cosmetec.it', 'giuliastrada@cosmetec.it'],
+                 'COSMONDE':['jana.wirt@cosmonde.cz', 'eliska.kockova@cosmonde.cz'],
+                 'DELIA':['skupinskak@delia.pl', 'klaudia.stoinska@delia.pl'],
+                 'DONAUKANOL':['eldina.takmak@donau-kanol.com','sarah.prentner@donau-kanol.com'],
+                 'FABER-CAST':'karin.puerner@fc-cosmetics.com',
+                 'FIABILA':['stephanie.hardouin-letellier@fiabila.com','cindy.berthelot@fiabila.com'],
+                 'ICC': ['m.lodolo@icc-italy.com','f.bellavita@icc-italy.com'],
+                 'ITIT':['s.marzorati@ititcosmetics.it', 's.puce@ititcosmetics.it'],
+                 'JOVI': ['tkostova@jovi.es', 'cscos@jovi.es'],
+                 'PHARMA COS': ['michela.bruno@pharmacos.it', 'isabelle.diliddo@pharmacos.it'],
+                 'PHARMACF':['joanna.kostrz@pharmacf.com.pl', 'olga.wesolowska@pharmacf.com.pl'],
+                 'R&D COLOR': ['alessandrabertagna@redcolor.it', 'robertabattarola@redcolor.it', 'jessicacioccolini@redcolor.it'],
+                 'STEP': 'customercare@stepcosmetici.com',
+                 'OXY':['eekinci@oxygendevelopment.com', 'emea-specifications-de@oxygendevelopment.com', 'ablunck@oxygendevelopment.com']}
+
     cc = ['christoph.razek@emea-cosmetics.com','dzanana.dautefendic@emea-cosmetics.com']
 
-    #print(df_samples.to_markdown())
 
 
-    #Reminder für alle Artikel für die Samples benötigt werden und noch nicht eingetroffen sind
+
+    #Auffüllen der Zellen ohne Info ob erhalten
     df_samples['PE14_SampleReceived'] = df_samples['PE14_SampleReceived'].fillna('0000-00-00 00:00:00')
     df_samples['Today'] = today
     df_samples['Today'] = pd.to_datetime(df_samples['Today'])
+    #Ermittlung Differenz von Lieferdatum zu heute
     df_samples['diff_days'] = (df_samples['LIEFERDATUM'] - df_samples['Today']) / np.timedelta64(1, 'D')
 
-    # ZEITDIFFERENZ NOCH ZU ERMITTELN!!!!!!
-    #Prüfung und EMAIL wenn KEINE Reminder
-
+    #Vorabmusterpflicht, keine Erhalt-Info und Liefertermin in 14 fällig
     df_reminder = df_samples[(df_samples['VorabM_Pflicht'] == 1) & (df_samples['PE14_SampleReceived'] == '0000-00-00 00:00:00') & (df_samples['diff_days'] < 14)]
     companies = set(df_reminder['SUCHNAME'].tolist())
 
     #Reminder für jede Firma einzeln
     for c in companies:
-
+        # Wenn kein Reminder notwendig: info an User
         if len(companies) == 0:
             # creating an win32 object/mail object
             outlook = win32.Dispatch('outlook.application')
@@ -63,7 +81,10 @@ def send_reminder(df_samples):
             outlook = win32.Dispatch('outlook.application')
             mail = outlook.CreateItem(0)
 
-            mail.To = receivers[c]
+            if type(receivers[c])!= list:
+                mail.To = receivers[c]
+            else:
+                mail.To = ";".join(receivers[c])
             mail.CC = ";".join(cc)
             mail.Subject = f'Reminder for Sample: {c}'
             mail.HTMLBody = """<font face='Calibri, Calibri, monospace'>
@@ -73,15 +94,23 @@ def send_reminder(df_samples):
             If you have any questions please feel free to contact me (yian.su@emea-cosmetics.com).<br><br>
             Thank you and kind regards.<br>
             <br>
-            Yian
-            </font>"""
+            Yian<br>
+            <img src='S:/EMEA/Kontrollabfragen/VorabM_Reminder/Logo.png' width='200' height='100'>
+            <br>
+            emea Handelsgesellschaft mbH<br>
+            Brucknerstraße 8/5<br>
+            A-1040 Wien<br>
+            Tel.:    +43 1 535 10 01 - 232<br>
+            
+            </font>
+            """
             mail.Attachments.Add(rf'S:\EMEA\Kontrollabfragen\VorabM_Reminder\{c}_Sample_Reminder.xlsx')
 
             mail.Display()
             mail.Save()
             #mail.Send()
         else:
-            # creating an win32 object/mail object
+            # Wenn keine Email hinterlegt ist, info an Entwickler
             outlook = win32.Dispatch('outlook.application')
             mail = outlook.CreateItem(0)
 

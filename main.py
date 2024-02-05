090cf94e83ddebc6787387942688bec8767fa2b6
ChristophRazek <52331031+ChristophRazek@users.noreply.github.com>
2024-02-05 09:26:07 +0100

    anpassung in der logik, wenn kein Reminder verschickt wird. Abfragebedingung muss außerhalb der for schleife für die firmen liegen

--- main.py
+++ main.py
@@ -52,75 +52,79 @@ def send_reminder(df_samples):
     df_reminder = df_samples[(df_samples['VorabM_Pflicht'] == 1) & (df_samples['PE14_SampleReceived'] == '0000-00-00 00:00:00') & (df_samples['diff_days'] < 14)]
     companies = set(df_reminder['SUCHNAME'].tolist())
 
-    #Reminder für jede Firma einzeln
-    for c in companies:
-        # Wenn kein Reminder notwendig: info an User
-        if len(companies) == 0:
-            # creating an win32 object/mail object
-            outlook = win32.Dispatch('outlook.application')
-            mail = outlook.CreateItem(0)
+    #print(len(companies))
+    #breakpoint()
+    # Wenn kein Reminder notwendig: info an User
+    if len(companies) == 0:
+        # creating an win32 object/mail object
+        outlook = win32.Dispatch('outlook.application')
+        mail = outlook.CreateItem(0)
 
-            mail.To = ['christoph.razek@emea-cosmetics.com','yian.su@emea-cosmetics.com']
-            mail.Subject = f'Kein Reminder am {today} versendet, da keine Liefertermine anstehen'
+        mail.To = ";".join(cc)
+        mail.Subject = f'Kein Reminder am {today} versendet, da keine Liefertermine anstehen'
 
-            mail.Display()
-            mail.Save()
-            # mail.Send()
-
-        elif c in receivers:
-            df_attachmment = df_reminder[df_reminder['SUCHNAME']== c].drop(['FIXPOSNR','BELEGART','VorabM_Pflicht',
-                                                                    'PE14_SampleReceived', 'Today', 'diff_days'], axis=1)
-            df_attachmment.rename(columns={'BELEGNR':'PO','SUCHNAME':'SUPPLIER', 'ARTIKELNR':'ARTICLE',
-                                   'BEZEICHNUNG':'DESCRIPTION', 'LIEFERDATUM':'DELIVERY-DATE' }, inplace=True)
-
-            #Zwischenspeichern für Attachment
-            df_attachmment.to_excel(rf'S:\EMEA\Kontrollabfragen\VorabM_Reminder\{c}_Sample_Reminder.xlsx', index=False)
+        mail.Display()
+        mail.Save()
+        # mail.Send()
+    else:
+        #Reminder für jede Firma einzeln
+        for c in companies:
 
 
-            # creating an win32 object/mail object
-            outlook = win32.Dispatch('outlook.application')
-            mail = outlook.CreateItem(0)
+            if c in receivers:
+                df_attachmment = df_reminder[df_reminder['SUCHNAME']== c].drop(['FIXPOSNR','BELEGART','VorabM_Pflicht',
+                                                                        'PE14_SampleReceived', 'Today', 'diff_days'], axis=1)
+                df_attachmment.rename(columns={'BELEGNR':'PO','SUCHNAME':'SUPPLIER', 'ARTIKELNR':'ARTICLE',
+                                       'BEZEICHNUNG':'DESCRIPTION', 'LIEFERDATUM':'DELIVERY-DATE' }, inplace=True)
 
-            if type(receivers[c])!= list:
-                mail.To = receivers[c]
+                #Zwischenspeichern für Attachment
+                df_attachmment.to_excel(rf'S:\EMEA\Kontrollabfragen\VorabM_Reminder\{c}_Sample_Reminder.xlsx', index=False)
+
+
+                # creating an win32 object/mail object
+                outlook = win32.Dispatch('outlook.application')
+                mail = outlook.CreateItem(0)
+
+                if type(receivers[c])!= list:
+                    mail.To = receivers[c]
+                else:
+                    mail.To = ";".join(receivers[c])
+                mail.CC = ";".join(cc)
+                mail.Subject = f'Reminder for Sample: {c}'
+                mail.HTMLBody = """<font face='Calibri, Calibri, monospace'>
+                Good Day, <br><br>
+                Please send us the Production Samples for the Articles in the list attached as the initial delivery dates will soon be reached.<br>
+                In case there are problems, please inform us as soon as possible.<br>
+                If you have any questions please feel free to contact me (yian.su@emea-cosmetics.com).<br><br>
+                Thank you and kind regards.<br>
+                <br>
+                Yian<br>
+                <img src='S:/EMEA/Kontrollabfragen/VorabM_Reminder/Logo.png' width='200' height='100'>
+                <br>
+                emea Handelsgesellschaft mbH<br>
+                Brucknerstraße 8/5<br>
+                A-1040 Wien<br>
+                Tel.:    +43 1 535 10 01 - 232<br>
+                
+                </font>
+                """
+                mail.Attachments.Add(rf'S:\EMEA\Kontrollabfragen\VorabM_Reminder\{c}_Sample_Reminder.xlsx')
+
+                mail.Display()
+                mail.Save()
+                #mail.Send()
             else:
-                mail.To = ";".join(receivers[c])
-            mail.CC = ";".join(cc)
-            mail.Subject = f'Reminder for Sample: {c}'
-            mail.HTMLBody = """<font face='Calibri, Calibri, monospace'>
-            Good Day, <br><br>
-            Please send us the Production Samples for the Articles in the list attached as the initial delivery dates will soon be reached.<br>
-            In case there are problems, please inform us as soon as possible.<br>
-            If you have any questions please feel free to contact me (yian.su@emea-cosmetics.com).<br><br>
-            Thank you and kind regards.<br>
-            <br>
-            Yian<br>
-            <img src='S:/EMEA/Kontrollabfragen/VorabM_Reminder/Logo.png' width='200' height='100'>
-            <br>
-            emea Handelsgesellschaft mbH<br>
-            Brucknerstraße 8/5<br>
-            A-1040 Wien<br>
-            Tel.:    +43 1 535 10 01 - 232<br>
-            
-            </font>
-            """
-            mail.Attachments.Add(rf'S:\EMEA\Kontrollabfragen\VorabM_Reminder\{c}_Sample_Reminder.xlsx')
+                # Wenn keine Email hinterlegt ist, info an Entwickler
+                outlook = win32.Dispatch('outlook.application')
+                mail = outlook.CreateItem(0)
 
-            mail.Display()
-            mail.Save()
-            #mail.Send()
-        else:
-            # Wenn keine Email hinterlegt ist, info an Entwickler
-            outlook = win32.Dispatch('outlook.application')
-            mail = outlook.CreateItem(0)
-
-            mail.To = 'christoph.razek@emea-cosmetics.com'
-            mail.Subject = f'Fehlende Email Adresse {c} für Vorabmuster'
+                mail.To = 'christoph.razek@emea-cosmetics.com'
+                mail.Subject = f'Fehlende Email Adresse {c} für Vorabmuster'
 
 
-            mail.Display()
-            mail.Save()
-            #mail.Send()
+                mail.Display()
+                mail.Save()
+                #mail.Send()
 
 
 
